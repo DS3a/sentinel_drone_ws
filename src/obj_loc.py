f6592ff6b350eb46c3c2a0eb4263abfe337ffc2d
DS3a <33351122+DS3a@users.noreply.github.com>
2022-11-25 13:48:42 +0530

    added spiral code

--- src/obj_loc.py
+++ src/obj_loc.py
@@ -36,7 +36,7 @@ class Edrone():
 		self.drone_position = [0.0,0.0,0.0]
 
 		# [x_setpoint, y_setpoint, z_setpoint]
-		self.setpoint = [-6,6,15] # whycon marker at the position of the dummy given in the scene. Make the whycon marker associated with position_to_hold dummy renderable and make changes accordingly
+		self.setpoint = [0,0,12] # whycon marker at the position of the dummy given in the scene. Make the whycon marker associated with position_to_hold dummy renderable and make changes accordingly
 
 
 		#Declaring a cmd of message type edrone_msgs and initializing values
